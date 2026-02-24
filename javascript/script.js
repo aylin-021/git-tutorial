0410159824f3a06a08458257e4b3b329dda3df45
aylin-021 <aylin.sh.oooo@gmail.com>
2026-02-24 14:04:45 +0330

    روز دوم

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,84 @@
-let numbers = document.querySelectorAll(".number");  
-let zarbBtn = document.querySelector(".zarb"); 
-let resultBtn = document.querySelector(".result"); 
-let display = document.querySelector(".natije"); 
+let number = document.querySelectorAll(".number");
+let zarb = document.querySelector(".zarb");
+let result = document.querySelector(".result");
+let natije = document.querySelector(".natije");
+let sum = document.querySelector(".sum");
+let menha = document.querySelector(".menha");
+let taghsim = document.querySelector(".taghsim");
+let clear = document.querySelector(".clear");
 
-let Input = ""; 
+let zakhireMeghdarFeli = "";
+let zakhireMeghdarGhabli = "";
+let zakhireAmalgareEntekhabi = "";
 
-numbers.forEach(function(item) {
-    item.addEventListener("click", function() {
-        Input += item.innerText;  
-        document.querySelector(".natije").innerText = Input;
-    });
+number.forEach(item => {
+    item.addEventListener("click", function () {
+        zakhireMeghdarFeli += item.textContent;
+        natije.textContent = zakhireMeghdarFeli;
+    })
+});
+
+
+zarb.addEventListener("click", function () {
+    if (zakhireMeghdarFeli !== "") {
+        zakhireMeghdarGhabli = zakhireMeghdarFeli;
+        zakhireMeghdarFeli = "";
+        zakhireAmalgareEntekhabi = "*";
+    }
+});
+
+sum.addEventListener("click", function () {
+    if (zakhireMeghdarFeli !== "") {
+        zakhireMeghdarGhabli = zakhireMeghdarFeli;
+        zakhireMeghdarFeli = "";
+        zakhireAmalgareEntekhabi = "+";
+    }
+});
+
+menha.addEventListener("click", function () {
+    if (zakhireMeghdarFeli !== "") {
+        zakhireMeghdarGhabli = zakhireMeghdarFeli;
+        zakhireMeghdarFeli = "";
+        zakhireAmalgareEntekhabi = "-";
+    }
+});
+
+taghsim.addEventListener("click", function () {
+    if (zakhireMeghdarFeli !== "") {
+        zakhireMeghdarGhabli = zakhireMeghdarFeli;
+        zakhireMeghdarFeli = "";
+        zakhireAmalgareEntekhabi = "/";
+    }
+})
+
+clear.addEventListener("click", function () {
+    zakhireMeghdarGhabli = "";
+    zakhireMeghdarFeli = "";
+    zakhireAmalgareEntekhabi = "";
+    natije.textContent = "0";
+});
+
+result.addEventListener("click", function () {
+    if (zakhireMeghdarFeli !== "" && zakhireMeghdarGhabli !== "") {
+        let resultt = 0;
+        const feli = parseFloat(zakhireMeghdarFeli);
+        const ghabli = parseFloat(zakhireMeghdarGhabli);
+
+        switch (zakhireAmalgareEntekhabi) {
+            case "+":
+                resultt = ghabli + feli;
+                break;
+            case "-":
+                resultt = ghabli - feli;
+                break;
+            case "*":
+                resultt = ghabli * feli;
+                break;
+            case "/":
+                resultt = ghabli / feli;
+                break;
+        }
+
+        natije.textContent = resultt;
+    }
 });
